0079cea0dfd380207f6d985afc959b094007bdba
Arvind Nataraj <arvindnata123@gmail.com>
2016-05-22 22:46:16 +0530

    First commit

--- airq_sensors_standalone.c
+++ airq_sensors_standalone.c
@@ -6,7 +6,7 @@
  Copyright (c) 2011-2014 Analog Devices, Inc.
 
 All rights reserved.
-
+Hello !! Cool
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
   - Redistributions of source code must retain the above copyright notice,
